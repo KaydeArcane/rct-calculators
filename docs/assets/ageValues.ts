8602af29e608c30c679116210aba83229b8a2d22
Kayde Arcane <kayde.arcane@gmail.com>
2020-09-07 19:51:47 -0500

    created production build in docs folder

---
+++ docs/assets/ageValues.ts
@@ -0,0 +1,62 @@
+export const ageValues = [
+  {
+    "id": 0,
+    "age": "Less than 5 months",
+    "multiplier": 1.5,
+    "classicBonus": 30
+  },
+  {
+    "id": 1,
+    "age": "Between 5 and 13 months",
+    "multiplier": 1.2,
+    "classicBonus": 10
+  },
+  {
+    "id": 2,
+    "age": "Between 13 and 40 months",
+    "multiplier": 1,
+    "classicBonus": 0
+  },
+  {
+    "id": 3,
+    "age": "Between 40 and 64 months",
+    "multiplier": 0.75,
+    "classicBonus": 0
+  },
+  {
+    "id": 4,
+    "age": "Between 64 and 88 months",
+    "multiplier": 0.56,
+    "classicBonus": 0
+  },
+  {
+    "id": 5,
+    "age": "Between 88 and 104 months",
+    "multiplier": 0.42,
+    "classicBonus": 0
+  },
+  {
+    "id": 6,
+    "age": "Between 104 and 120 months",
+    "multiplier": 0.32,
+    "classicBonus": 0
+  },
+  {
+    "id": 7,
+    "age": "Between 120 and 128 months",
+    "multiplier": 0.16,
+    "classicBonus": 0
+  },
+  {
+    "id": 8,
+    "age": "Between 128 and 200 months",
+    "multiplier": 0.08,
+    "classicBonus": 0
+  },
+  {
+    "id": 9,
+    "age": "More than 200 months",
+    "multiplier": 0.56,
+    "classicBonus": 0
+  },
+];
